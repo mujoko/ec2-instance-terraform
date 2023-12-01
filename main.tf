159566a494ce349cc2b71db7c1f3c879728784eb
Mujoko <mujoko.mujoko@gmail.com>
2023-12-01 10:31:17 +0700

    Update main.tf
    remove GS

--- main.tf
+++ main.tf
@@ -13,9 +13,6 @@ resource "aws_instance" "web-server" {
   ami = "${var.ami}"
   instance_type = "${var.instance_type}"
   availability_zone = "${var.availability_zone}"
-  vpc_security_group_ids        = [
-        "${var.vpc_rds_security_group_id}"
-    ]
   subnet_id = "${var.subnet_id}"
   key_name = "${var.key_name}"
   disable_api_termination = "true"
